3c6015dbd8081197244b4d776a17499d712bc525
finn-01 <nguyensam576@gmail.com>
2022-04-13 01:32:24 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import { Navbar } from "./components";
+
 const App = () => {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return <Navbar />;
 };
 
 export default App;
